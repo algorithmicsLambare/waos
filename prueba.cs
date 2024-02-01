768e9cb22729c0c97a4ed115600b6257cd69d132
algorithmicsLambare <158515196+algorithmicsLambare@users.noreply.github.com>
2024-02-01 17:12:44 -0300

    Add files via upload
    guanpa guanpa yico yico

---
+++ prueba.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class prueba : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        //se va a imprimir hola mundo en la consola
+        print("hello world");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
